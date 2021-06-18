2babfca3aa1eb3b468ec9aa5e9ed93bbdc1aad33
Caique Portela <caiquesportela@gmail.com>
2021-06-18 19:54:09 -0300

    Ajustes na imagem do php 7.4-dev

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 docker build -t php-7.4-dev php/7.4-dev/
-docker tag php-7.4-dev:latest caiquesporte/php:7.4-dev
+docker tag php-7.4-dev:latest caiquesportela/php:7.4-dev
+
+docker build -t php-8.0-dev php/8.0-dev/
+docker tag php-8.0-dev:latest caiquesportela/php:8.0-dev
